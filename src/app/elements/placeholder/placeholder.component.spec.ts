1e801447fb5799a339c02a02ca8b0caa65cedc64
gunnerr91 <blissvortex@hotmail.com>
2021-05-03 15:51:54 +0100

    added unit tests for placeholder

--- src/app/elements/placeholder/placeholder.component.spec.ts
+++ src/app/elements/placeholder/placeholder.component.spec.ts
@@ -1,25 +1,49 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PlaceholderComponent } from './placeholder.component';
 
 describe('PlaceholderComponent', () => {
   let component: PlaceholderComponent;
   let fixture: ComponentFixture<PlaceholderComponent>;
+  let de: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlaceholderComponent ]
-    })
-    .compileComponents();
+      declarations: [PlaceholderComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlaceholderComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('renders header if header attribute is set to true', () => {
+    component.header = true;
+    fixture.detectChanges();
+    expect(de.query(By.css('.header'))).toBeTruthy();
+  });
+
+  it('does not render header if header attribute is set to false', () => {
+    component.header = false;
+    fixture.detectChanges();
+    expect(de.query(By.css('.header'))).toBeFalsy();
+  });
+
+  // it('renders lines specified by input props', () => {
+  //   component.lines = 15;
+  //   fixture.detectChanges();
+  //   expect(de.query(By.css('.paragraph')).nativeElement.innerHTML.length).toBe(
+  //     15
+  //   );
+  // });
 });
--- src/app/elements/times.directive.spec.ts
+++ src/app/elements/times.directive.spec.ts
@@ -1,8 +1,83 @@
+import {
+  ComponentFactory,
+  ComponentRef,
+  ElementRef,
+  EmbeddedViewRef,
+  Injector,
+  NgModuleRef,
+  TemplateRef,
+  ViewContainerRef,
+  ViewRef,
+} from '@angular/core';
 import { TimesDirective } from './times.directive';
 
 describe('TimesDirective', () => {
   it('should create an instance', () => {
-    const directive = new TimesDirective();
+    const directive = new TimesDirective(
+      new TestViewContainerRef(),
+      new TestTemplateRef()
+    );
     expect(directive).toBeTruthy();
   });
 });
+
+class TestTemplateRef extends TemplateRef<any> {
+  get elementRef(): ElementRef<any> {
+    throw new Error('Method not implemented.');
+  }
+  createEmbeddedView(context: any): EmbeddedViewRef<any> {
+    throw new Error('Method not implemented.');
+  }
+}
+
+class TestViewContainerRef extends ViewContainerRef {
+  get element(): ElementRef<any> {
+    throw new Error('Method not implemented.');
+  }
+  get injector(): Injector {
+    throw new Error('Method not implemented.');
+  }
+  get parentInjector(): Injector {
+    throw new Error('Method not implemented.');
+  }
+  clear(): void {
+    throw new Error('Method not implemented.');
+  }
+  get(index: number): ViewRef | null {
+    throw new Error('Method not implemented.');
+  }
+  get length(): number {
+    throw new Error('Method not implemented.');
+  }
+  createEmbeddedView<C>(
+    templateRef: TemplateRef<C>,
+    context?: C,
+    index?: number
+  ): EmbeddedViewRef<C> {
+    throw new Error('Method not implemented.');
+  }
+  createComponent<C>(
+    componentFactory: ComponentFactory<C>,
+    index?: number,
+    injector?: Injector,
+    projectableNodes?: any[][],
+    ngModule?: NgModuleRef<any>
+  ): ComponentRef<C> {
+    throw new Error('Method not implemented.');
+  }
+  insert(viewRef: ViewRef, index?: number): ViewRef {
+    throw new Error('Method not implemented.');
+  }
+  move(viewRef: ViewRef, currentIndex: number): ViewRef {
+    throw new Error('Method not implemented.');
+  }
+  indexOf(viewRef: ViewRef): number {
+    throw new Error('Method not implemented.');
+  }
+  remove(index?: number): void {
+    throw new Error('Method not implemented.');
+  }
+  detach(index?: number): ViewRef | null {
+    throw new Error('Method not implemented.');
+  }
+}
