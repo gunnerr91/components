49cc671f979dd8331ade39015e233e8d33f09f89
gunnerr91 <blissvortex@hotmail.com>
2021-05-08 14:50:08 +0100

    added views home and statistics components

---
+++ src/app/views/views-home/views-home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-views-home',
+  templateUrl: './views-home.component.html',
+  styleUrls: ['./views-home.component.css'],
+})
+export class ViewsHomeComponent implements OnInit {
+  stats = [
+    { value: 22, label: '# of users' },
+    { value: 900, label: 'Revenue' },
+    { value: 50, label: 'Satisfaction' },
+  ];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
